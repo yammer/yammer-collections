7a5c5fdd77e6b253d6784c3ff2a4484e644de146
Michał Rutkowski <mrutkowski@yammer-inc.com>
2013-10-28 20:47:27 -0700

    simplified transforming set and its test: now it inherits from transforming collection, and the test only coveres additional implementation

--- azure-table-core/src/main/java/com/yammer/collections/guava/azure/transforming/TransformingSet.java
+++ azure-table-core/src/main/java/com/yammer/collections/guava/azure/transforming/TransformingSet.java
@@ -1,18 +1,10 @@
 package com.yammer.collections.guava.azure.transforming;
 
 import com.google.common.base.Function;
-import com.google.common.collect.Iterators;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import java.util.AbstractSet;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Objects;
 import java.util.Set;
 
-public class TransformingSet<F, T> extends AbstractSet<F> implements Set<F> {
-    private Collection<F> collectionWhichIsASet;
+public class TransformingSet<F, T> extends TransformingCollection<F, T> implements Set<F> {
 
     /**
      * This implementation will break if the following is not satisfied:
@@ -23,55 +15,12 @@ public class TransformingSet<F, T> extends AbstractSet<F> implements Set<F> {
      * i.e., fromFunction is a bijection and the toFunction is its reverse
      */
     public TransformingSet(Set<T> backingSet, Function<F, T> toFunction, Function<T, F> fromFunction) {
-        collectionWhichIsASet = new TransformingCollection<>(backingSet, toFunction, fromFunction);
-    }
-
-    @Override
-    public int size() {
-        return collectionWhichIsASet.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return collectionWhichIsASet.isEmpty();
-    }
-
-    @Override
-    public boolean contains(Object o) {
-            return collectionWhichIsASet.contains(o);
-    }
-
-    @Override
-    public Iterator<F> iterator() {
-        return collectionWhichIsASet.iterator();
-    }
-
-    @Override
-    public boolean add(F f) {
-        return collectionWhichIsASet.add(f);
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return collectionWhichIsASet.remove(o);
-    }
-
-    @Override
-    public void clear() {
-        collectionWhichIsASet.clear();
-    }
-
-    @Override
-    public int hashCode() {
-        return collectionWhichIsASet.hashCode();
+        super(backingSet, toFunction, fromFunction);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == null) { return false; }
-        if (o == this) { return true; }
-        if (!(o instanceof Set)) {return false;}
+        return o != null && (o == this || o instanceof Set && super.equals(o));
 
-        return collectionWhichIsASet.equals(o);
     }
 }
